d6cac409a0def8ecf48084401e17452feec2fc53
poojakavitha <52231190+poojakavitha@users.noreply.github.com>
2019-07-03 05:54:20 -0700

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+x=input()
+x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U':
+  print"Vowel"
+elif x=='$':
+  print"invalid"
+else:
+  print"Consonant"
