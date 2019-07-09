0fae0bf1943fd41265fc382d052d42587ba127c0
poojakavitha <52231190+poojakavitha@users.noreply.github.com>
2019-07-09 17:31:10 +0530

    Update 16p.py

--- 16p.py
+++ 16p.py
@@ -1,4 +1,10 @@
 a,b=map(str,input().split())
 if len(a)!=len(b):
-  print "no"
-n=
+  print("no")
+else:
+  d1=[a.count(i) for i in a]
+  d2=[b.count(i) for i in b]
+if d1==d2:
+  print("Yes")
+else:
+  peint("no")
