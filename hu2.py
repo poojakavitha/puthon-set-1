229e470b1fce6de7156c7c9775c833a62c8dae1a
poojakavitha <52231190+poojakavitha@users.noreply.github.com>
2019-08-14 06:03:49 -0700

    Create hu2.py

---
+++ hu2.py
@@ -0,0 +1,12 @@
+arr=[[]
+n=int(input())
+for i in range(0,n):
+  ele=int(input())
+    arr.append(ele)
+size=len(arr)
+arr.sort(reverse=True)
+num=arr[0]
+for i in range(1,size):
+    num=num*10+arr[i]
+print(num,end=" ")
+  
